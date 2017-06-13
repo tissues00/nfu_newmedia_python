d50d790e1024adbf6c032caabdd3b4c43319d24c
123rainbow123 <1306349650@qq.com>
2017-06-13 11:55:11 +0800

    Create country.py

--- nfu_newmedia.national.data/webapp_pick_a_national/country.py
+++ nfu_newmedia.national.data/webapp_pick_a_national/country.py
@@ -2,21 +2,21 @@
 # 类化，以备模块
 
 """创建一个类"""
-class country_list_name (object):
+class national_list_name (object):
 
-    def __init__(self, fn='data\country.tsv'):
+    def __init__(self, fn='data\national.tsv'):
        import csv
        with open(fn, 'r', encoding='utf8') as csvfile:
            reader = csv.DictReader(csvfile, fieldnames=['c_code', 'c_name'], delimiter='\t')
            fieldnames = reader.fieldnames
 
-           list_dict_country = []
+           list_dict_national = []
            for row in reader:
-                  list_dict_country.append(dict(row))
+                  list_dict_national.append(dict(row))
 
-           self.data = {d['c_code']:d['c_name'] for d in list_dict_country}
+           self.data = {d['c_code']:d['c_name'] for d in list_dict_national}
 
-    def country_name(self, c_code=''):
+    def national_name(self, c_code=''):
         c_name =  self.data.get(c_code, None)
         return (c_name)
 
