77e38690abd78df6d319aca89103196b82a940c1
123rainbow123 <1306349650@qq.com>
2017-06-15 22:13:13 +0800

    Create generate_list_national_03_class_using.py

---
+++ nfu_newmedia.national.data/webapp_pick_a_national/generate_list_national_03_class_using.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+# 使用模块module country
+import national 
+c = national.national_list_name()
+
+# 測試   
+print (c.national_name('CN'))
+print (c.national_name('SG'))
+print (c.national_name('ZZ'))
+print (c.national_name('ABC'))
+print (c.national_name(''))
+ 
