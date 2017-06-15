b6216efdf6f29db42fa2797648e8171df80282d0
123rainbow123 <1306349650@qq.com>
2017-06-15 22:10:17 +0800

    Create generate_list_national_02_module_using.py

---
+++ nfu_newmedia.national.data/webapp_pick_a_national/generate_list_national_02_module_using.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*- 
+# 使用模块module glcountry
+
+import glcountry
+print (glcountry.national_name('CN'))
+print (glcountry.national_name('SG'))
+print (glcountry.national_name('ZZ'))
+print (glcountry.national_name('ABC'))
+print (glcountry.national_name(''))
+
